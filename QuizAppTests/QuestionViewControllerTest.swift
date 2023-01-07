f41ecc16d2d9580afe9dc624469d39f8284b5375
Patrick <champion790713@gmail.com>
2023-01-07 12:11:13 +0800

    [QuestionVC] tableview tests

--- QuizAppTests/QuestionViewControllerTest.swift
+++ QuizAppTests/QuestionViewControllerTest.swift
@@ -9,29 +9,49 @@ import XCTest
 final class QuestionViewControllerTest: XCTestCase {
 
     func test_viewDidLoad_renderQuestionHeaderText() {
-        XCTAssertEqual(makeSUT(options: []).headerLabel.text, "Q1")
+        XCTAssertEqual(makeSUT(question: "Q1", options: ["A1"]).headerLabel.text, "Q1")
     }
 
-    func test_viewDidLoad_render_Options() {
+    func test_viewDidLoad_rendersOptions() {
         XCTAssertEqual(makeSUT(options: []).tableView.numberOfRows(inSection: 0), 0)
         XCTAssertEqual(makeSUT(options: ["A1"]).tableView.numberOfRows(inSection: 0), 1)
         XCTAssertEqual(makeSUT(options: ["A1", "A2"]).tableView.numberOfRows(inSection: 0), 2)
     }
     
-    func test_viewDidLoad_withOneOption_renderOneOptionText() {
-        let sut = makeSUT(options: ["A1"])
-
-        let indexPath = IndexPath(row: 0, section: 0)
-        let cell = sut.tableView.dataSource?.tableView(sut.tableView, cellForRowAt: indexPath)
+    func test_viewDidLoad_rendersOptionsText() {
+        XCTAssertEqual(makeSUT(options: ["A1"]).tableView.title(at: 0), "A1")
+        XCTAssertEqual(makeSUT(options: ["A1", "A2"]).tableView.title(at: 1), "A2")
+    }
+    
+    func test_optionSelected_notifiesDelegate() {
+        var receivedAnswer = ""
+        let sut = makeSUT(options: ["A1"]) {
+            receivedAnswer = $0
+        }
         
-        XCTAssertEqual(cell?.textLabel?.text, "A1")
+        let indexPath = IndexPath(row: 0, section: 0)
+        sut.tableView.delegate?.tableView?(sut.tableView, didSelectRowAt: indexPath)
+        XCTAssertEqual(receivedAnswer, "A1")
     }
     
     // MARK: Helpers
-    func makeSUT(question: String = "", options: [String]) -> QuestionViewController {
-        let sut = QuestionViewController(question: question, options: options)
+    func makeSUT(question: String = "",
+                 options: [String],
+                 selection: @escaping (String) -> Void = { _ in}) -> QuestionViewController {
+        let sut = QuestionViewController(question: question, options: options, selection: selection)
 
         _ = sut.view
         return sut
+    }
+}
+
+private extension UITableView {
+    
+    func cell(at row: Int) -> UITableViewCell? {
+        return dataSource?.tableView(self, cellForRowAt: IndexPath(row: row, section: 0))
+    }
+    
+    func title(at row: Int) -> String? {
+        return cell(at: row)?.textLabel?.text
     }
 }
