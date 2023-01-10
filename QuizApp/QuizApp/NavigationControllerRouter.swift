2f57923f69f0d3530c0816c1a11440743b07e927
Patrick <champion790713@gmail.com>
2023-01-10 15:19:23 +0800

    [Nav tests] fake nav push test

--- QuizApp/QuizApp/NavigationControllerRouter.swift
+++ QuizApp/QuizApp/NavigationControllerRouter.swift
@@ -5,16 +5,23 @@
 import UIKit
 import QuizEngine
 
+protocol ViewControllerFactory {
+    func questionViewController(for question: String, answerCallback: @escaping (String) -> Void) -> UIViewController
+}
+
 class NavigationControllerRouter: Router {
     
     private let navigationController: UINavigationController
+    private let factory: ViewControllerFactory
     
-    init(_ navigationController: UINavigationController) {
+    init(_ navigationController: UINavigationController, facotry: ViewControllerFactory) {
         self.navigationController = navigationController
+        self.factory = facotry
     }
     
     func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
-        navigationController.pushViewController(.init(), animated: false)
+        let viewController = factory.questionViewController(for: question, answerCallback: answerCallback)
+        navigationController.pushViewController(viewController, animated: true)
     }
     
     func routeTo(result: Result<String, String>) {
